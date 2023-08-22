a280e60fd28811201a94f39d6eafc070ebdf9a71
keshav gautam <66683243+keshavgautam1@users.noreply.github.com>
2023-08-23 00:05:41 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const app = express();
+const port = 3000;
+const expressLayouts = require('express-ejs-layouts');
+const db = require('./config/mongoose');
+//used for session cookie
+const session = require('express-session');
+const passport = require('passport');
+const passportLocal = require('./config/passport-local-strategy'); //this is the strategy that we have defined
+// const MongoStore = require('connect-mongo')(session); //this is used to store the session cookie in the db
+
+const MongoStore = require('connect-mongo'); // new version of connect-mongo
+// Create a new instance of MongoStore
+const sessionStore = new MongoStore({
+    mongoUrl: 'mongodb://127.0.0.1:27017/Socionize_development',
+    mongooseConnection: db,
+    autoRemove: 'disabled'
+});
+// Handle error during session store creation
+sessionStore.on('error', function(err) {
+    console.log('Error in session store:', err);
+});
+
+app.use(express.urlencoded()); //middleware to parse the form data that is submitted by the user
+app.use(cookieParser());
+
+app.use(express.static('./assets'));
+
+app.use(expressLayouts);
+//extract style and scripts from sub pages into the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
+//set up the view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+// mongo store is used to store the session cookie in the db 
+app.use(session({
+    name: 'socionize',
+   // TODO change the secret before deployment in production mode
+    secret: "blahsomthing",
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+        maxAge: (1000 * 60 * 100)
+    },
+    store: sessionStore //this is the session store that we have defined above
+    
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(passport.setAuthenticatedUser);
+
+//use express router
+app.use('/', require('./routes'));
+
+
+// Start the server
+app.listen(port, function(err){
+    if (err){
+        console.log(`Error in running the server: ${err}`);
+    }else{
+        console.log(`Server is running on port: ${port}`);
+    }
+});
